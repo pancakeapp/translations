05c97fe88c8dad9a52102d968177bc978967693a
1Martin <albruna@albruna.nl>
2014-03-08 16:27:31 +0100

    language update to pancake 4.1.15

---
+++ dutch/ion_auth_lang.php
@@ -0,0 +1,47 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+* Name:   Ion Auth Lang - German
+* 
+* Author: Martin Kapma
+*		  http://www.albruna.nl
+*		  albruna@albruna.nl
+*         @albruna_nl
+*
+* Created: 06.03.2014
+*
+* Description: Dutch language file for Ion Auth messages and errors
+* Beschrijving: Nederlandstalig bestand voor Ion Auth System- en foutmeldingen
+*
+*/
+
+// Account Creation
+$lang['account_creation_successful'] 	  	 = 'Account aanmaken is gelukt';
+$lang['account_creation_unsuccessful'] 	 	 = 'Niet mogelijk een account aan te maken';
+$lang['account_creation_duplicate_email'] 	 = 'E-mail is al in gebruik of ongeldig';
+$lang['account_creation_duplicate_username'] 	 = 'Gebruikersnaam is al in gebruik of ongeldig';
+
+
+// Password
+$lang['password_change_successful'] 	 	 = 'Wachtwoord is aangepast';
+$lang['password_change_unsuccessful'] 	  	 = 'Niet mogelijk wachtwoord aan te passen';
+$lang['forgot_password_successful'] 	 	 = 'Wachtwoord is gereset, e-mail is verzonden';
+$lang['forgot_password_unsuccessful'] 	 	 = 'Niet mogelijk wachtwoord te resetten';
+
+// Activation
+$lang['activate_successful'] 		  	 = 'Account is geactiveerd';
+$lang['activate_unsuccessful'] 		 	 = 'Niet mogelijk account te activeren';
+$lang['deactivate_successful'] 		  	 = 'Account is gedeactiveerd';
+$lang['deactivate_unsuccessful'] 	  	 = 'Niet mogelijk account te deactiveren';
+$lang['activation_email_successful'] 	  	 = 'Activatie e-mail is verzonden';
+$lang['activation_email_unsuccessful']   	 = 'Niet mogelijk activatie e-mail te versturen';
+
+// Login / Logout
+$lang['login_successful'] 		  	 = 'Met succes ingelogd';
+$lang['login_unsuccessful'] 		  	 = 'Onjuiste Login';
+$lang['logout_successful'] 		 	 = 'Met succes uitgelogd';
+  
+// Account Changes
+$lang['update_successful'] 		 	 = 'Account informatie is bijgewerkt';
+$lang['update_unsuccessful'] 		 	 = 'Kan accountinformatie niet bijwerken';
+$lang['delete_successful'] 		 	 = 'Gebruiker is verwijderd';
+$lang['delete_unsuccessful'] 		 	 = 'Kan gebruiker niet verwijderen';
