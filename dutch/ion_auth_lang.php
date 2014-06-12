50c43c86a6865294941143e8ddc0ec536c61ad2b
Peter Ruiter <peterruiter@gmail.com>
2014-06-12 09:48:51 +0200

    Update ion_auth_lang.php

--- dutch/ion_auth_lang.php
+++ dutch/ion_auth_lang.php
@@ -1,39 +1,41 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
-* Name:   Ion Auth Lang - German
+* Name:  Ion Auth Lang - Dutch
+*
+* Author: Ben Edmunds
+* 		  ben.edmunds@gmail.com
+*         @benedmunds
 * 
-* Author: Martin Kapma
-*		  http://www.albruna.nl
-*		  albruna@albruna.nl
-*         @albruna_nl
-*
-* Created: 06.03.2014
-*
-* Description: Dutch language file for Ion Auth messages and errors
-* Beschrijving: Nederlandstalig bestand voor Ion Auth System- en foutmeldingen
-*
+* Location: http://github.com/benedmunds/ion_auth/
+* 
+* Translated by: Peter Ruiter, connectedconcepts.nl, based on first translation of Yvonne Emmerig, Dymago.nl
+* 
+* Created:  	 14.12.2012 
+* 
+* Description:  Dutch language file for Ion Auth messages and errors - Nederlandse vertaling voor Ion Auth berichten en foutmeldingen
 */
 
 // Account Creation
 $lang['account_creation_successful'] 	  	 = 'Account aanmaken is gelukt';
 $lang['account_creation_unsuccessful'] 	 	 = 'Niet mogelijk een account aan te maken';
-$lang['account_creation_duplicate_email'] 	 = 'E-mail is al in gebruik of ongeldig';
+$lang['account_creation_duplicate_email'] 	 = 'Email is al in gebruik of ongeldig';
 $lang['account_creation_duplicate_username'] 	 = 'Gebruikersnaam is al in gebruik of ongeldig';
 
 
 // Password
 $lang['password_change_successful'] 	 	 = 'Wachtwoord is aangepast';
 $lang['password_change_unsuccessful'] 	  	 = 'Niet mogelijk wachtwoord aan te passen';
-$lang['forgot_password_successful'] 	 	 = 'Wachtwoord is gereset, e-mail is verzonden';
+$lang['forgot_password_successful'] 	 	 = 'Wachtwoord is gereset, email is verzonden';
 $lang['forgot_password_unsuccessful'] 	 	 = 'Niet mogelijk wachtwoord te resetten';
+$lang['cant_send_email'] = "Er is een probleem opgetreden bij het verzenden van een email.";
 
 // Activation
 $lang['activate_successful'] 		  	 = 'Account is geactiveerd';
 $lang['activate_unsuccessful'] 		 	 = 'Niet mogelijk account te activeren';
 $lang['deactivate_successful'] 		  	 = 'Account is gedeactiveerd';
 $lang['deactivate_unsuccessful'] 	  	 = 'Niet mogelijk account te deactiveren';
-$lang['activation_email_successful'] 	  	 = 'Activatie e-mail is verzonden';
-$lang['activation_email_unsuccessful']   	 = 'Niet mogelijk activatie e-mail te versturen';
+$lang['activation_email_successful'] 	  	 = 'Activatie email is verzonden';
+$lang['activation_email_unsuccessful']   	 = 'Niet mogelijk activatie email te versturen';
 
 // Login / Logout
 $lang['login_successful'] 		  	 = 'Met succes ingelogd';
