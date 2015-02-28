7a6240834b7e41c29f336a26ab928c87f2eab14b
Bruno Moreira De Barros <bruno@terraduo.com>
2015-02-27 23:42:40 -0500

    [German] Automated commit updating error:response_will_be_sent_to_email.

--- german/pancake_lang.php
+++ german/pancake_lang.php
@@ -1396,3 +1396,4 @@ $lang["invoices:partially_paid"] = "Teilweise";
 $lang["error:cannot_send_error_details"] = "Sie können keine Fehlerdetails schicken, um Pancake Unterstützung, weil Sie sind nicht angemeldet.";
 $lang["error:unknown_error_reporting"] = "Ein unbekannter Fehler aufgetreten. Bitte melden Sie diesen Fehler manuell.";
 $lang["error:sending_details"] = "Senden Einzelheiten ...";
+$lang["error:response_will_be_sent_to_email"] = "Sie werden Antworten auf {email} benachrichtigt.";
