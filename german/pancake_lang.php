6c4d8e237e0b1a01ed012c0e1add8ef572de0d92
Pancake Translations <support@pancakeapp.com>
2015-04-15 19:35:59 -0400

    [German] Automated commit updating projects:role_add_task.

--- german/pancake_lang.php
+++ german/pancake_lang.php
@@ -1418,3 +1418,4 @@ $lang["tickets:unarchive"] = "Wiederherstellen";
 $lang["projects:role_delete"] = "Löschen";
 $lang["projects:role_delete_task"] = "Aufgabe löschen";
 $lang["projects:role_edit_task"] = "Aufgabe bearbeiten";
+$lang["projects:role_add_task"] = "Aufgabe hinzufügen";
