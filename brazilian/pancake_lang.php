ce6941758b694fde3029b755f930064f23cce52c
Pancake Translations <support@pancakeapp.com>
2015-04-16 18:11:59 -0400

    [Brazilian] Automated commit updating clients:view.

--- brazilian/pancake_lang.php
+++ brazilian/pancake_lang.php
@@ -1272,3 +1272,4 @@ $lang["credit_notes:preview"] = "Pré-visualizar Nota de Crédito";
 $lang["invoices:make_bulk_payment"] = "Faça Pagamento de Grande Volume";
 $lang["credit_notes:duplicated"] = "Nota de crédito duplicada #:1. O novo número da nota de crédito é #:2.";
 $lang["credit_notes:client_viewed"] = ": 1 <a href=\":2\">nota de crédito visualizada #: 3</a> em: 4";
+$lang["clients:view"] = "Ver Cliente";
