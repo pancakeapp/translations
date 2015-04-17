2fb37540a7e614ce28e567db446807ac2bafa411
Pancake Translations <support@pancakeapp.com>
2015-04-17 08:05:37 -0400

    [Brazilian] Automated commit updating settings:error_reported.

--- brazilian/pancake_lang.php
+++ brazilian/pancake_lang.php
@@ -1353,3 +1353,4 @@ $lang["error:not_logged_in_intro"] = "Esses erros são registrados automaticamen
 $lang["error:unknown_error_reporting"] = "Ocorreu um erro desconhecido. Por favor, reporte este erro manualmente.";
 $lang["error:no_logged_errors"] = "Você não tem erros registrados.";
 $lang["error:cannot_send_error_details"] = "Você não pode enviar detalhes do erro para o Pancake Support, porque você não está logado.";
+$lang["settings:error_reported"] = "Erro Reportado";
