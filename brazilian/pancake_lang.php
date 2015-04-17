b154c6409d98f186d6650a79cf4dfbaac05ee77b
Pancake Translations <support@pancakeapp.com>
2015-04-17 08:06:37 -0400

    [Brazilian] Automated commit updating tickets:archive.

--- brazilian/pancake_lang.php
+++ brazilian/pancake_lang.php
@@ -1365,3 +1365,4 @@ $lang["settings:view_error_details"] = "Ver Detalhes do Erro";
 $lang["global:x_times"] = ":1 de vezes";
 $lang["clients:noclientbodyfiltered"] = "Nenhum dos nomes dos seus clientes começa com ':1'.";
 $lang["settings:delete_error"] = "Apagar o Erro";
+$lang["tickets:archive"] = "Arquivo";
