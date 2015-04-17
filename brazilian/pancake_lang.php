863493ddb782ac7c8d452bb1e01410e09b4addf4
Pancake Translations <support@pancakeapp.com>
2015-04-17 07:57:09 -0400

    [Brazilian] Automated commit updating clients:has_no_unpaid_invoices.

--- brazilian/pancake_lang.php
+++ brazilian/pancake_lang.php
@@ -1297,3 +1297,4 @@ $lang["settings:hide_tax_column"] = "Esconder a Coluna de Impostos?";
 $lang["tickets:not_yet_generated"] = "A fatura ainda não foi gerada";
 $lang["clients:credit_balance_currently"] = "Saldo de Crédito (atualmente% 1)";
 $lang["invoices:send_bulk_payment_notification"] = "Enviar e-mails de notificação de pagamento para o cliente?";
+$lang["clients:has_no_unpaid_invoices"] = ":1 não tem faturas a serem pagas.";
