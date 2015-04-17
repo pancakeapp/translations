dceb22a9fd5238d963c7a33839df41edd6c3c382
Pancake Translations <support@pancakeapp.com>
2015-04-17 06:16:10 -0400

    [Brazilian] Automated commit updating credit_notes:no_credit_notes.

--- brazilian/pancake_lang.php
+++ brazilian/pancake_lang.php
@@ -1277,3 +1277,4 @@ $lang["invoices:x_invoices"] = ":1 faturas";
 $lang["invoices:store_bulk_payment"] = "Pagamento à loja cumulativo";
 $lang["invoices:amount_to_pay"] = "Montante para marcar como pago";
 $lang["credit_notes:create_credit_note"] = "Criar Nota de Crédito";
+$lang["credit_notes:no_credit_notes"] = "Você não tem notas de crédito.";
