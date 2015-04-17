296f5b6353367261e6dd395e254096c6ef734ac1
Pancake Translations <support@pancakeapp.com>
2015-04-17 08:05:24 -0400

    [Brazilian] Automated commit updating error:unknown_error_reporting.

--- brazilian/pancake_lang.php
+++ brazilian/pancake_lang.php
@@ -1350,3 +1350,4 @@ $lang["invoices:bulk_payment_added"] = "O pagamento em massa foi adicionado com 
 $lang["reports:verb_created"] = "Criado";
 $lang["dashboard:view_all_upcoming_invoices"] = "Ver Todas as Próximas Faturas";
 $lang["error:not_logged_in_intro"] = "Esses erros são registrados automaticamente.";
+$lang["error:unknown_error_reporting"] = "Ocorreu um erro desconhecido. Por favor, reporte este erro manualmente.";
