a4a3bee08b0ba1484e987fdeb1f0982a0bba175a
Pancake Translations <support@pancakeapp.com>
2015-04-17 08:05:33 -0400

    [Brazilian] Automated commit updating error:cannot_send_error_details.

--- brazilian/pancake_lang.php
+++ brazilian/pancake_lang.php
@@ -1352,3 +1352,4 @@ $lang["dashboard:view_all_upcoming_invoices"] = "Ver Todas as Próximas Faturas"
 $lang["error:not_logged_in_intro"] = "Esses erros são registrados automaticamente.";
 $lang["error:unknown_error_reporting"] = "Ocorreu um erro desconhecido. Por favor, reporte este erro manualmente.";
 $lang["error:no_logged_errors"] = "Você não tem erros registrados.";
+$lang["error:cannot_send_error_details"] = "Você não pode enviar detalhes do erro para o Pancake Support, porque você não está logado.";
