dcfea56d2fb05bad5e04ac7f2d56636bbbb1acda
Pancake Translations <support@pancakeapp.com>
2015-04-18 09:11:18 -0400

    [French] Automated commit updating proposals:list_accepted.

--- french/pancake_lang.php
+++ french/pancake_lang.php
@@ -1632,3 +1632,4 @@ $lang["proposals:list_archived"] = "Propositions archivées";
 $lang["invoices:invoices_archived"] = "Factures archivées";
 $lang["invoices:estimates_archived"] = "Estimations Archivées";
 $lang["invoices:archived"] = "Vous avez archivé la facture #:1 avec succès.";
+$lang["proposals:list_accepted"] = "Propositions acceptées";
