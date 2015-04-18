812bec6884fb8ce1b81462b4cdf7bea9e7195280
Pancake Translations <support@pancakeapp.com>
2015-04-18 09:08:55 -0400

    [French] Automated commit updating proposals:list_all.

--- french/pancake_lang.php
+++ french/pancake_lang.php
@@ -1627,3 +1627,4 @@ $lang["credit_notes:archived"] = "Vous avez archivé la note de crédit #:1 avec
 $lang["estimates:restored"] = "Vous avez restauré l'estimation #:1 avec succès.";
 $lang["credit_notes:restored"] = "Vous avez restauré la note de crédit #:1 avec succès.";
 $lang["proposals:list_rejected"] = "Offres rejetées";
+$lang["proposals:list_all"] = "Toutes les offres.";
