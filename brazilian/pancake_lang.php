1d5197bee77592a49c60a9e01777bf1955770f02
Pancake Translations <support@pancakeapp.com>
2015-04-18 10:13:02 -0400

    [Brazilian] Automated commit updating users:confirm_password.

--- brazilian/pancake_lang.php
+++ brazilian/pancake_lang.php
@@ -1499,3 +1499,4 @@ $lang["global:bcc_was_sent_to"] = "Este e-mail foi enviado para % 1 em % 2.";
 $lang["tasks:task_notes"] = "Notas de tarefas";
 $lang["error:php_outdated"] = "A versão do PHP que você está usando está desatualizada.";
 $lang["error:pancake_will_stop_supporting_it"] = "O Pancake vai deixar de prestar suporte a ele em algumas semanas.";
+$lang["users:confirm_password"] = "Confirme Senha";
