2c45d261fd948dc527a4b47b9fd0d6e019aa97a7
Pancake Translations <support@pancakeapp.com>
2015-04-18 09:08:40 -0400

    [French] Automated commit updating credit_notes:restored.

--- french/pancake_lang.php
+++ french/pancake_lang.php
@@ -1625,3 +1625,4 @@ $lang["estimates:archived"] = "Vous avez archivé l'estimation #:1 avec succès.
 $lang["invoices:credit_notes_archived"] = "Notes de Crédit archivées";
 $lang["credit_notes:archived"] = "Vous avez archivé la note de crédit #:1 avec succès.";
 $lang["estimates:restored"] = "Vous avez restauré l'estimation #:1 avec succès.";
+$lang["credit_notes:restored"] = "Vous avez restauré la note de crédit #:1 avec succès.";
