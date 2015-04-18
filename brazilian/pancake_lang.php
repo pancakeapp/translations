446316b441dd850b0d747f6423b0694e93cb1b06
Pancake Translations <support@pancakeapp.com>
2015-04-18 10:11:48 -0400

    [Brazilian] Automated commit updating global:by_x_with_time.

--- brazilian/pancake_lang.php
+++ brazilian/pancake_lang.php
@@ -1484,3 +1484,4 @@ $lang["users:please_enter_information"] = "Por favor digite as informações do 
 $lang["users:edit_user"] = "Editar Usuário";
 $lang["global:download_csv"] = "Baixar o CSV";
 $lang["tasks:complete_this_task"] = "Complete esta tarefa";
+$lang["global:by_x_with_time"] = "Por 1%, 2%";
