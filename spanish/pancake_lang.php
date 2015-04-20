03c1bf3629107eabebce26b9622ed03c8067dc92
Pancake Translations <support@pancakeapp.com>
2015-04-20 04:50:44 -0400

    [Spanish] Add invoices:see_payment_schedule_below.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -1199,5 +1199,6 @@ Facturas sin pagar o pagadas parcialmente son mostradas de acuerdo a la fecha de
   'error:cant_send_email_solutions' => 'Si tiene acceso de administrador (root) al servidor, asegúrese de tener sendmail instalado y trate de encontrarlo con <code>sendmail</code>.',
   'error:not_enough_disk_space_solutions' => 'Si tiene acceso root al servidor de la base de datos, pruebe vaciando /tmp. Si eso no funciona, revise el uso del espacio del disco duro en su servidor de base de datos, y trate de liberar un poco de espacio del disco duro.',
   'error:page_does_not_exist' => 'La página a la que estaba tratando de acceder no existe.',
+  'invoices:see_payment_schedule_below' => 'Ver Programa de Pagos debajo',
 );
 /** End of file: pancake_lang.php **/
