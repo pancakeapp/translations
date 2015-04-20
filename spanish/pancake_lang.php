46d8f47f5a43edff0cbe3f825da3beeeaed19ddf
Pancake Translations <support@pancakeapp.com>
2015-04-20 04:33:05 -0400

    [Spanish] Add credit_notes:credit_note_date.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -1040,5 +1040,6 @@ Facturas sin pagar o pagadas parcialmente son mostradas de acuerdo a la fecha de
   'credit_notes:no_credit_notes_body' => '¡Deberías apuntarte a eso! ¿Te gustaría crear una ahora?',
   'credit_notes:send_now' => 'Enviar la nota de crédito',
   'credit_notes:credit_note_number' => 'Nota de Crédito #% 1',
+  'credit_notes:credit_note_date' => 'Fecha de la nota de crédito',
 );
 /** End of file: pancake_lang.php **/
