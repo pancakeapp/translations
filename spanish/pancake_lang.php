9ab5c37afbd575335e97fa54c821e891f8ae442b
Pancake Translations <support@pancakeapp.com>
2015-04-20 04:34:59 -0400

    [Spanish] Add credit_notes:client_viewed.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -1054,5 +1054,6 @@ Facturas sin pagar o pagadas parcialmente son mostradas de acuerdo a la fecha de
   'credit_notes:addedconf' => 'Ha añadido la nota de crédito # <strong>:1</strong>, por un total de <strong>:2</strong> (:3 con impuestos) para % 4% 5.',
   'invoices:send_bulk_payment_notification' => '¿Enviar notificaciones de correos electrónicos de los pagos al cliente?',
   'global:credit_balance' => 'Balance de crédito',
+  'credit_notes:client_viewed' => '% 1 <a href=":2">nota de crédito vista # 3%</a> en 4%',
 );
 /** End of file: pancake_lang.php **/
