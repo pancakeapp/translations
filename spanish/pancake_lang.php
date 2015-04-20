679fb7c3a4a786c48b8a94454f61db6c5c6c6838
Pancake Translations <support@pancakeapp.com>
2015-04-20 04:41:13 -0400

    [Spanish] Add global:triennially.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -1093,5 +1093,6 @@ Facturas sin pagar o pagadas parcialmente son mostradas de acuerdo a la fecha de
   'clients:no_amount_specified' => 'No has especificado una cantidad.',
   'clients:tax_registration_number' => ':1 Número / ID',
   'expenses:receipt' => 'Recibo',
+  'global:triennially' => 'Trienalmente',
 );
 /** End of file: pancake_lang.php **/
