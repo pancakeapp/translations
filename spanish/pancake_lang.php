02adadd3e9e8597724a559c2c3d1bd45f992d404
Pancake Translations <support@pancakeapp.com>
2015-04-20 04:44:53 -0400

    [Spanish] Add error:cannot_send_error_details.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -1139,5 +1139,6 @@ Facturas sin pagar o pagadas parcialmente son mostradas de acuerdo a la fecha de
   'error:response_will_be_sent_to_email' => 'Se le notificarán las respuestas en {correo electrónico}.',
   'projects:role_view' => 'Ver',
   'settings:error_reported' => 'Error reportado',
+  'error:cannot_send_error_details' => 'No puedes enviar los detalles del error al equipo de soporte de Pancake porque no estás conectado.',
 );
 /** End of file: pancake_lang.php **/
