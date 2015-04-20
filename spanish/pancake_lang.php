bef211e5b8bab5a9f78ebd4260bd7c1f5775517f
Pancake Translations <support@pancakeapp.com>
2015-04-20 04:43:06 -0400

    [Spanish] Add error:subtitle.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -1116,5 +1116,6 @@ Facturas sin pagar o pagadas parcialmente son mostradas de acuerdo a la fecha de
   'clients:tax_numbers' => 'Número de identificación fiscal de los clientes / documentos de identidad',
   'dashboard:there_are_no_upcoming_invoices' => 'No hay nuevas facturas para mostrar.',
   'dashboard:view_all_upcoming_invoices' => 'Ver todas las facturas pendientes',
+  'error:subtitle' => 'Se ha producido un error desconocido.',
 );
 /** End of file: pancake_lang.php **/
