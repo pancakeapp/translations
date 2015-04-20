57f4b213a421cb5b0e7a5c295ef2b7c6bb02d8d5
Pancake Translations <support@pancakeapp.com>
2015-04-20 04:41:27 -0400

    [Spanish] Add clients:what_do_you_want_to_do.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -1096,5 +1096,6 @@ Facturas sin pagar o pagadas parcialmente son mostradas de acuerdo a la fecha de
   'global:triennially' => 'Trienalmente',
   'invoices:pancake_will_automatically_change_is_viewable' => 'Esto cambiará a "sí" automáticamente cuando se envíe  la factura al cliente por correo electrónico.',
   'expenses:category_added' => '¡Éxito! La categoría de gastos fue creada satisfactoriamente.',
+  'clients:what_do_you_want_to_do' => '¿Qué quiere hacer?',
 );
 /** End of file: pancake_lang.php **/
