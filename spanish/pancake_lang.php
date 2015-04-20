dcb7fde870d33daada0f8811807640c21c871b61
Pancake Translations <support@pancakeapp.com>
2015-04-20 04:44:10 -0400

    [Spanish] Add global:twice.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -1129,5 +1129,6 @@ Facturas sin pagar o pagadas parcialmente son mostradas de acuerdo a la fecha de
   'global:once' => 'Una vez',
   'settings:delete_error' => 'Eliminar error',
   'global:x_times' => ':1 veces',
+  'global:twice' => 'Dos veces',
 );
 /** End of file: pancake_lang.php **/
