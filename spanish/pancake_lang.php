b290e28fde71bef2e0f1e279c97d100445cc131b
Pancake Translations <support@pancakeapp.com>
2015-04-20 04:40:27 -0400

    [Spanish] Add clients:add_custom_field.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -1084,5 +1084,6 @@ Facturas sin pagar o pagadas parcialmente son mostradas de acuerdo a la fecha de
   'global:rejected' => 'Rechazado',
   'estimates:rejected' => 'Rechazos estimados',
   'items:fixed_discount' => 'Descuento (:1)',
+  'clients:add_custom_field' => 'Añadir otro campo personalizado',
 );
 /** End of file: pancake_lang.php **/
