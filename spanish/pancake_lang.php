d069c516ddac85734a25ccef235e3c50aecdc327
Pancake Translations <support@pancakeapp.com>
2015-04-20 04:50:11 -0400

    [Spanish] Add error:page_not_found.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -1195,5 +1195,6 @@ Facturas sin pagar o pagadas parcialmente son mostradas de acuerdo a la fecha de
   'error:no_tmp_dir_permissions_explanation' => 'Su servidor está configurado de tal manera que no permite a Pancake crear archivos temporales en <code>1%</code>.',
   'invoices:see_payment_schedule' => 'Ver Programa de Pagos',
   'times.label.task_id' => 'Tarea:',
+  'error:page_not_found' => 'Página no encontrada',
 );
 /** End of file: pancake_lang.php **/
