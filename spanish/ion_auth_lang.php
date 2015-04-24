e36c3f6547bbb5b0c7b3c96bd221d2b37163180f
Pancake Translations <support@pancakeapp.com>
2015-04-24 10:44:32 -0400

    [Spanish] Add email_forgotten_password_subject.

--- spanish/ion_auth_lang.php
+++ spanish/ion_auth_lang.php
@@ -1,48 +1,44 @@
-<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
 /**
-* Name:  Ion Auth Lang - English
-* 
-* Author: Francisco Arenas
-* 		  francisco.arenas@dospuntocero.cl
-*         @dospuntocero
-* 
-* Location: https://github.com/dospuntocero/translations/
-*          
-* Created:  03.14.2010 
-* 
-* Description:  Spanish language file for Ion Auth messages and errors
-* 
-*/
+ * Pancake
+ *
+ * A simple, fast, self-hosted invoicing application.
+ *
+ * This is the Spanish translation for Pancake.
+ *
+ * @package     Pancake
+ * @author      Francisco arenas <francisco.arenas@dospuntocero.cl> - dospuntocero.cl
+ * @copyright   Copyright (c) 2015, Pancake Payments
+ * @license     https://www.pancakeapp.com/license
+ * @link        https://www.pancakeapp.com
+ * @since       Version 4.8.10
+ */
 
-// Account Creation
-$lang['account_creation_successful'] 	  	 = 'Cuenta creada exitosamente';
-$lang['account_creation_unsuccessful'] 	 	 = 'No es posible crear una cuenta';
-$lang['account_creation_duplicate_email'] 	 = 'Email está siendo utilizado o es inválido';
-$lang['account_creation_duplicate_username'] 	 = 'Nombre de usuario en uso o inválido';
+// ------------------------------------------------------------------------
 
-
-// Password
-$lang['password_change_successful'] 	 	 = 'Contraseña cambiada exitosamente';
-$lang['password_change_unsuccessful'] 	  	 = 'No es posible cambiar contraseña';
-$lang['forgot_password_successful'] 	 	 = 'Mail de reseteo de contraseña enviado';
-$lang['forgot_password_unsuccessful'] 	 	 = 'No es posible resetear la contraseña';
-$lang['cant_send_email'] = "Un problema ha ocurrido mientras se trataba de enviar un mail.";
-
-// Activation
-$lang['activate_successful'] 		  	 = 'Cuenta activada';
-$lang['activate_unsuccessful'] 		 	 = 'No es posible activar la cuenta';
-$lang['deactivate_successful'] 		  	 = 'Cuenta desactivada';
-$lang['deactivate_unsuccessful'] 	  	 = 'No es posible desactivar la cuentaUnable to De-Activate Account';
-$lang['activation_email_successful'] 	  	 = 'Email de activación enviado';
-$lang['activation_email_unsuccessful']   	 = 'No es posible enviar el mail de activación';
-
-// Login / Logout
-$lang['login_successful'] 		  	 = 'Conexión realizada';
-$lang['login_unsuccessful'] 		  	 = 'Ingreso incorrecto';
-$lang['logout_successful'] 		 	 = 'Desconectado exitosamente';
-  
-// Account Changes
-$lang['update_successful'] 		 	 = 'Información de la cuenta actualizada exitosamente';
-$lang['update_unsuccessful'] 		 	 = 'No es posible actualizar la información de la cuenta';
-$lang['delete_successful'] 		 	 = 'Usuario borrado';
-$lang['delete_unsuccessful'] 		 	 = 'No es posible eliminar al usuario';
+$lang = array (
+  'account_creation_successful' => 'Cuenta creada exitosamente',
+  'account_creation_unsuccessful' => 'No es posible crear una cuenta',
+  'account_creation_duplicate_email' => 'Email está siendo utilizado o es inválido',
+  'account_creation_duplicate_username' => 'Nombre de usuario en uso o inválido',
+  'password_change_successful' => 'Contraseña cambiada exitosamente',
+  'password_change_unsuccessful' => 'No es posible cambiar contraseña',
+  'forgot_password_successful' => 'Mail de reseteo de contraseña enviado',
+  'forgot_password_unsuccessful' => 'No es posible resetear la contraseña',
+  'cant_send_email' => 'Un problema ha ocurrido mientras se trataba de enviar un mail.',
+  'activate_successful' => 'Cuenta activada',
+  'activate_unsuccessful' => 'No es posible activar la cuenta',
+  'deactivate_successful' => 'Cuenta desactivada',
+  'deactivate_unsuccessful' => 'No es posible desactivar la cuentaUnable to De-Activate Account',
+  'activation_email_successful' => 'Email de activación enviado',
+  'activation_email_unsuccessful' => 'No es posible enviar el mail de activación',
+  'login_successful' => 'Conexión realizada',
+  'login_unsuccessful' => 'Ingreso incorrecto',
+  'logout_successful' => 'Desconectado exitosamente',
+  'update_successful' => 'Información de la cuenta actualizada exitosamente',
+  'update_unsuccessful' => 'No es posible actualizar la información de la cuenta',
+  'delete_successful' => 'Usuario borrado',
+  'delete_unsuccessful' => 'No es posible eliminar al usuario',
+  'email_forgotten_password_subject' => 'Verificación de contraseña olvidada',
+);
+/** End of file: ion_auth_lang.php **/
