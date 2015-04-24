0f582e3c4e0161a8f12eb1f2c6e501f02eae673e
Pancake Translations <support@pancakeapp.com>
2015-04-24 11:19:49 -0400

    [Spanish] Add proposals:list_rejected.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -1292,5 +1292,6 @@ $lang = array (
   'error:scan_pancake_explanation' => 'A veces podrían surgir problemas a causa de una actualización fallida o debido a los problemas de permisos que causan que tus archivos de Pancake se corrompan durante una actualización. Usa esto para comprobar tu Pancake ante cualquier tipo de problemas.',
   'proposals:list_archived' => 'Propuestas Archivadas',
   'proposals:list_unanswered' => 'Propuestas sin respuesta. ',
+  'proposals:list_rejected' => 'Propuestas Rechazadas',
 );
 /** End of file: pancake_lang.php **/
