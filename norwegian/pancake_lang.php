3b4bc9210e092412c1b9b944cf7fa559f35ec12e
Pancake Translations <support@pancakeapp.com>
2015-04-24 16:25:10 -0400

    [Norwegian] Add proposals:archived.

--- norwegian/pancake_lang.php
+++ norwegian/pancake_lang.php
@@ -1276,5 +1276,6 @@ $lang = array (
   'error:click_here_for_instructions' => 'Klikk her for instruksjoner',
   'error:scan_result_failure_how_to_fix' => 'For å løse disse problemene, må du manuelt oppdatere filene til din Pancake.',
   'error:and_x_others' => '... og % 1 andre.',
+  'proposals:archived' => 'Du har arkivert forslag #:1.',
 );
 /** End of file: pancake_lang.php **/
