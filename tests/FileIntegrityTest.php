ab16bbaece5fe6a5f045f9723a17d60bcf67203f
Bruno Moreira De Barros <bruno@terraduo.com>
2015-04-24 12:58:59 +0100

    Changes the test to exclude README.txt. Adds checks for missing keys and for broken variables.

--- tests/FileIntegrityTest.php
+++ tests/FileIntegrityTest.php
@@ -22,7 +22,7 @@ class FileIntegrityTest extends PHPUnit_Framework_TestCase {
         $ignore[] = 'index.html';
         $ignore[] = 'vendor';
         $ignore[] = 'tests';
-        $ignore[] = 'README.md';
+        $ignore[] = 'README.txt';
         $ignore[] = 'phpunit.xml';
 
         foreach (scandir($folder) as $file) {
@@ -39,11 +39,13 @@ class FileIntegrityTest extends PHPUnit_Framework_TestCase {
     }
 
     public function provider() {
-        $base = dirname(__FILE__)."/../";
+        $base = dirname(__FILE__) . "/../";
         $buffer = $this->get_recursive_file_list($base);
         $return = array();
         foreach ($buffer as $file) {
-            $return[] = array($base.$file);
+            $lang_file = explode("/", $file, 2);
+            $english_lang_file = "english/" . $lang_file[1];
+            $return[] = array($base . $file, $base, $base . $english_lang_file);
         }
         return $return;
     }
@@ -51,10 +53,33 @@ class FileIntegrityTest extends PHPUnit_Framework_TestCase {
     /**
      * @dataProvider provider
      */
-    public function testFilesCanBeParsedAndHaveLangArray($file) {
-        require_once $file;
-        $this->assertTrue(isset($lang), "A \$lang variable should be set.");
-        $this->assertTrue(is_array($lang), "The \$lang variable should be an array.");
+    public function testFilesCanBeParsedAndHaveLangArray($file, $base, $english_file) {
+        require $english_file;
+        $english_lang = $lang;
+
+        ob_start();
+        require $file;
+        $contents = ob_get_contents();
+        ob_end_clean();
+        $short_file = str_ireplace($base, '', $file);
+        $this->assertTrue(isset($lang), "A \$lang variable should be set (in $short_file).");
+        $this->assertTrue(is_array($lang), "The \$lang variable should be an array (in $short_file).");
+        $this->assertTrue(empty($contents), "Loading a language file should not produce any output whatsoever.");
+
+        foreach ($english_lang as $key => $value) {
+            $this->assertTrue(isset($lang[$key]), "The \$lang variable should have a '$key' key (in $short_file).");
+            $variables_in_english = [];
+            $variables_in_new_language = [];
+            $count_matches_in_english = preg_match_all("/((?::[0-9]+)|(?:{[^}]+}))/u", $value, $variables_in_english);
+            $count_matches_in_new_language = preg_match_all("/((?::[0-9]+)|(?:{[^}]+}))/u", $lang[$key], $variables_in_new_language);
+
+            $this->assertEquals($count_matches_in_new_language, $count_matches_in_english, "The number of variables in '$key' key (in $short_file) don't match what's expected.");
+
+            foreach ($variables_in_english as $i => $variable) {
+                $this->assertEquals($variables_in_new_language[$i], $variable, "One of the variables in '$key' key (in $short_file) doesn't match what's expected.");
+            }
+        }
+
     }
 
 }
