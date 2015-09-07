a4843034b28e9aa61242f64dde897fae84d1bb59
Pancake Translations <support@pancakeapp.com>
2015-09-07 17:26:08 -0400

    [Spanish] Update error:update_system_solutions.

--- spanish/pancake_lang.php
+++ spanish/pancake_lang.php
@@ -13,7 +13,7 @@
  * @copyright   Copyright (c) 2015, Pancake Payments
  * @license     https://www.pancakeapp.com/license
  * @link        https://www.pancakeapp.com
- * @since       Version 4.8.29
+ * @since       Version 4.8.36
  */
 
 // ------------------------------------------------------------------------
@@ -1304,7 +1304,7 @@ $lang = array (
   'error:update_system' => 'Error de actualización',
   'error:update_system_cannot_update_file' => 'Pancake fue incapaz de modificar uno o más de sus archivos durante la actualización.',
   'error:update_system_cannot_update' => 'Pancake fue incapaz de descargar la actualización correctamente.',
-  'error:update_system_solutions' => 'La actualización no se pudo completar, y su Pancake puede estar ahora en un estado parcialmente actualizado, lo que le causará errores. Tendrá que actualizar manualmente los archivos de su Pancake para resolver el problema. <br/><br/> :1 <br/><br/> Estos errores pueden ocurrir debido a un problema con los permisos de los archivos. Si Pancake no tiene acceso de escritura a todos sus archivos, es incapaz de actualizarse a sí mismo correctamente. En la mayoría de los servidores, esto se gestiona de forma automática, pero un pequeño número de servidores podría requerir intervención manual. Puede <a href="https://www.pancakeapp.com/account/support/ticket/new">iniciar un Ticket de ayuda de Pancake</a> si necesita ayuda averiguando lo que está mal.',
+  'error:update_system_solutions' => 'La actualización no se pudo completar, y su Pancake puede estar en estos momentos en un estado parcialmente actualizado, lo que le causará errores. Usted tendrá que actualizar manualmente sus archivos Pancake para resolver el problema. <br/><br/> :1 <br/><br/> Estos errores pueden ocurrir debido a un problema con los permisos de archivos. Si Pancake no tiene acceso de escritura a todos sus archivos, no es capaz de actualizarse a sí mismo correctamente. En la mayoría de los servidores, esto se maneja de forma automática, pero un pequeño número de servidores podría requerir intervención manual.',
   'error:update_without_ftp' => 'Si usted no tiene acceso al FTP (o no desea utilizar el FTP), pero tienes acceso al SSH de tu Pancake, <a class="blue-btn" href="https://www.pancakeapp.com/faq/update-without-ftp" target="_blank">has click aquí</a> para aprender cómo actualizar sin FTP.',
   'proposals:missing_client' => 'No has seleccionado un cliente.',
 );
