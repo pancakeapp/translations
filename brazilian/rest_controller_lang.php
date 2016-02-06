deea75650647a1fc11acc96b5f794019ba6fb5a8
Pancake Translations <support@pancakeapp.com>
2016-02-06 11:25:31 -0500

    [Brazilian] Add text_rest_ip_denied.

--- brazilian/rest_controller_lang.php
+++ brazilian/rest_controller_lang.php
@@ -19,5 +19,6 @@
 $lang = array (
   'text_rest_invalid_credentials' => 'Credenciais inválidas',
   'text_rest_invalid_api_key' => 'Chave de API %s inválida',
+  'text_rest_ip_denied' => 'IP negado',
 );
 /** End of file: rest_controller_lang.php **/
