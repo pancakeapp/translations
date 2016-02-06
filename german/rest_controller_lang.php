01dea139b849236e4a1eb2eef996b6674d234e6b
Pancake Translations <support@pancakeapp.com>
2016-02-06 12:26:12 -0500

    [German] Add text_rest_api_key_unauthorized.

--- german/rest_controller_lang.php
+++ german/rest_controller_lang.php
@@ -22,5 +22,6 @@ $lang = array (
   'text_rest_invalid_credentials' => 'Ungültige Anmeldedaten',
   'text_rest_ip_denied' => 'IP verweigert',
   'text_rest_ip_unauthorized' => 'IP unberechtigt',
+  'text_rest_api_key_unauthorized' => 'Dieser API-Schlüssel hat keinen Zugriff auf den gewünschten Controller',
 );
 /** End of file: rest_controller_lang.php **/
