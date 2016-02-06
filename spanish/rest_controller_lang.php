62d7dc512bf646fb72e3eac074f74277b7e11928
Pancake Translations <support@pancakeapp.com>
2016-02-06 11:27:16 -0500

    [Spanish] Add text_rest_api_key_unauthorized.

---
+++ spanish/rest_controller_lang.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Pancake
+ *
+ * A simple, fast, self-hosted invoicing application.
+ *
+ * This is the Spanish translation for Pancake.
+ *
+ * @package     Pancake
+
+ * @copyright   Copyright (c) 2016, Pancake Payments
+ * @license     https://www.pancakeapp.com/license
+ * @link        https://www.pancakeapp.com
+ * @since       Version 4.11.6
+ */
+
+// ------------------------------------------------------------------------
+
+$lang = array (
+  'text_rest_api_key_unauthorized' => 'Esta clave de API no tiene acceso al controlador solicitado.',
+);
+/** End of file: rest_controller_lang.php **/
