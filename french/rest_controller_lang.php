b65cc0b1b050e80c05815d5050751b61f6175d75
Pancake Translations <support@pancakeapp.com>
2016-02-06 11:43:05 -0500

    [French] Add text_rest_ajax_only.

--- french/rest_controller_lang.php
+++ french/rest_controller_lang.php
@@ -22,5 +22,6 @@ $lang = array (
   'text_rest_invalid_credentials' => ' Références non valides',
   'text_rest_ip_unauthorized' => 'IP non autorisée',
   'text_rest_unauthorized' => 'Non autorisé',
+  'text_rest_ajax_only' => 'Seules les requêtes AJAX sont autorisées',
 );
 /** End of file: rest_controller_lang.php **/
