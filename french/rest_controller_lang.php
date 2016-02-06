a88af8626d6da6cf491ccd733ab78add9ad457f2
Pancake Translations <support@pancakeapp.com>
2016-02-06 11:41:49 -0500

    [French] Add text_rest_ip_denied.

---
+++ french/rest_controller_lang.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Pancake
+ *
+ * A simple, fast, self-hosted invoicing application.
+ *
+ * This is the French translation for Pancake.
+ *
+ * @package     Pancake
+
+ * @copyright   Copyright (c) 2016, Pancake Payments
+ * @license     https://www.pancakeapp.com/license
+ * @link        https://www.pancakeapp.com
+ * @since       Version 4.11.6
+ */
+
+// ------------------------------------------------------------------------
+
+$lang = array (
+  'text_rest_ip_denied' => ' IP refusé',
+);
+/** End of file: rest_controller_lang.php **/
