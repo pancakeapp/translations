bf84d549767b3a6ad8e97275b30bbdfc93dca56c
Pancake Translations <support@pancakeapp.com>
2016-02-06 11:54:56 -0500

    [German] Add text_rest_invalid_api_key.

--- german/rest_controller_lang.php
+++ german/rest_controller_lang.php
@@ -7,7 +7,7 @@
  * This is the German translation for Pancake.
  *
  * @package     Pancake
-
+ * @author      Pancake Payments <support@pancakeapp.com> - https://www.pancakeapp.com
  * @copyright   Copyright (c) 2016, Pancake Payments
  * @license     https://www.pancakeapp.com/license
  * @link        https://www.pancakeapp.com
@@ -18,5 +18,6 @@
 
 $lang = array (
   'text_rest_unauthorized' => 'Unbefugt',
+  'text_rest_invalid_api_key' => 'Ungültiger API-Schlüssel %s',
 );
 /** End of file: rest_controller_lang.php **/
