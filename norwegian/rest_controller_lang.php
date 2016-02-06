d88486e329c5501f7c86672c761ac00a5f191d9d
Pancake Translations <support@pancakeapp.com>
2016-02-06 12:06:50 -0500

    [Norwegian] Add text_rest_invalid_api_key.

---
+++ norwegian/rest_controller_lang.php
@@ -0,0 +1,22 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+/**
+ * Pancake
+ *
+ * A simple, fast, self-hosted invoicing application.
+ *
+ * This is the Norwegian translation for Pancake.
+ *
+ * @package     Pancake
+
+ * @copyright   Copyright (c) 2016, Pancake Payments
+ * @license     https://www.pancakeapp.com/license
+ * @link        https://www.pancakeapp.com
+ * @since       Version 4.11.6
+ */
+
+// ------------------------------------------------------------------------
+
+$lang = array (
+  'text_rest_invalid_api_key' => 'Ugyldig API nøkkel %s',
+);
+/** End of file: rest_controller_lang.php **/
