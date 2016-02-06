abe853fc93a6f5c8c28ab0d82e6d88afb055296c
Pancake Translations <support@pancakeapp.com>
2016-02-06 11:42:53 -0500

    [French] Add text_rest_unauthorized.

--- french/rest_controller_lang.php
+++ french/rest_controller_lang.php
@@ -21,5 +21,6 @@ $lang = array (
   'text_rest_invalid_api_key' => 'Clé API non valide % s',
   'text_rest_invalid_credentials' => ' Références non valides',
   'text_rest_ip_unauthorized' => 'IP non autorisée',
+  'text_rest_unauthorized' => 'Non autorisé',
 );
 /** End of file: rest_controller_lang.php **/
