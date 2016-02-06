a8f2ea86f131f4964bb237846e372d99bea09f7b
Pancake Translations <support@pancakeapp.com>
2016-02-06 12:07:08 -0500

    [Norwegian] Add text_rest_ip_denied.

--- norwegian/rest_controller_lang.php
+++ norwegian/rest_controller_lang.php
@@ -7,7 +7,7 @@
  * This is the Norwegian translation for Pancake.
  *
  * @package     Pancake
-
+ * @author      Pancake Payments <support@pancakeapp.com> - https://www.pancakeapp.com
  * @copyright   Copyright (c) 2016, Pancake Payments
  * @license     https://www.pancakeapp.com/license
  * @link        https://www.pancakeapp.com
@@ -18,5 +18,6 @@
 
 $lang = array (
   'text_rest_invalid_api_key' => 'Ugyldig API nøkkel %s',
+  'text_rest_ip_denied' => 'IP benektet',
 );
 /** End of file: rest_controller_lang.php **/
