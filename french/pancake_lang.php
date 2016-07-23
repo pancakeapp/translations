f111fa97fc3d230fb6068d60215b6eaf0880d814
Pancake Translations <support@pancakeapp.com>
2016-07-23 15:30:30 -0400

    [French] Add invoices:charge_when_due.

--- french/pancake_lang.php
+++ french/pancake_lang.php
@@ -1320,5 +1320,6 @@ $lang = array (
   'invoices:auto_charge' => 'Facturer le Client Automatiquement',
   'invoices:pancake_will_charge_when_invoice_is_due' => 'Pancake débitera automatiquement ce client lorsque la facture sera due, en utilisant ses informations de paiement enregistrées.',
   'global:or' => 'ou',
+  'invoices:charge_when_due' => 'Facturer selon le dû',
 );
 /** End of file: pancake_lang.php **/
