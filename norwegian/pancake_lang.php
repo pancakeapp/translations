664cb7978e9e0ab534e457f72cf14e6be9f2b8bb
Pancake Translations <support@pancakeapp.com>
2016-07-23 18:20:45 -0400

    [Norwegian] Add invoices:pancake_will_charge_when_invoice_is_due.

--- norwegian/pancake_lang.php
+++ norwegian/pancake_lang.php
@@ -1317,5 +1317,6 @@ $lang = array (
   'invoice:description' => 'Beskrivelse',
   'invoices:charge_when_due' => 'Belast Ved Forfall',
   'invoices:auto_charge' => 'Belast Klienten Automatisk',
+  'invoices:pancake_will_charge_when_invoice_is_due' => 'Pancake vil automatisk belaste denne klienten når fakturaen forfaller, ved å benytte deres lagrede betalingsinformasjon.',
 );
 /** End of file: pancake_lang.php **/
